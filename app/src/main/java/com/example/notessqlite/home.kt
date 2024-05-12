c4e8eb90562bb8afa45300ef95d4305c4f6b8cb7
thathii <140898907+thathii@users.noreply.github.com>
2024-05-12 19:47:10 +0530

    home image add

--- app/src/main/java/com/example/notessqlite/home.kt
+++ app/src/main/java/com/example/notessqlite/home.kt
@@ -1,11 +1,21 @@
 package com.example.notessqlite
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val button = findViewById<Button>(R.id.homebutton)
+        button.setOnClickListener {
+            val intent = Intent(this, AddNoteActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
